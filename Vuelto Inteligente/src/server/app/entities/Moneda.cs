be445fab3c0250988a7b11c549f1666082caf662
drielnox <bl4z3r@gmail.com>
2016-08-13 21:33:57 -0300

    prueba: se puede crear una moneda con valor.

---
+++ Vuelto Inteligente/src/server/app/entities/Moneda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VueltoInteligente.Server.Application.Entities
+{
+    public class Moneda
+    {
+        private int v;
+
+        public Moneda(int v)
+        {
+            this.v = v;
+        }
+    }
+}
