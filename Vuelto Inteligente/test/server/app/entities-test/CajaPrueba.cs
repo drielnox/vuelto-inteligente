6782dc55d7b90f2f395e11a46c5a4516c410bf14
drielnox <bl4z3r@gmail.com>
2016-08-14 17:08:44 -0300

    prueba: puedo crear una caja y despues agregar varias monedas del mismo valor.

--- Vuelto Inteligente/test/server/app/entities-test/CajaPrueba.cs
+++ Vuelto Inteligente/test/server/app/entities-test/CajaPrueba.cs
@@ -79,5 +79,24 @@ namespace VueltoInteligente.Test.Server.Application.Entities
             Assert.IsNotNull(caja.ObtenerMoneda(moneda));
             Assert.IsTrue(caja.ObtenerMonedas().Count() == 1);
         }
+
+        [TestMethod]
+        public void PuedoCrearUnaCajaYDespuesAgregarVariasMonedasDelMismoValor()
+        {
+            var caja = new Caja();
+            var moneda1 = new Moneda(10);
+            var moneda2 = new Moneda(10);
+            var moneda3 = new Moneda(10);
+
+            caja.AgregarMoneda(moneda1);
+            caja.AgregarMoneda(moneda2);
+            caja.AgregarMoneda(moneda3);
+
+            Assert.IsNotNull(caja.ObtenerMoneda(moneda1));
+            Assert.IsNotNull(caja.ObtenerMoneda(moneda2));
+            Assert.IsNotNull(caja.ObtenerMoneda(moneda3));
+
+            Assert.IsTrue(caja.ObtenerMonedas().Count() == 3);
+        }
     }
 }
