9b807e87ab65662b155b4c3903fd821e5f0ba7d5
drielnox <bl4z3r@gmail.com>
2016-08-13 21:24:13 -0300

    Estructura basica del proyecto.

---
+++ Vuelto Inteligente/src/client/gui/console-app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VueltoInteligente.Client.Gui.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
