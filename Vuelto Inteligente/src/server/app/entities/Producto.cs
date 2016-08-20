b4a00dab5da8bafe408b484e54884ed480613874
drielnox <bl4z3r@gmail.com>
2016-08-20 16:15:38 -0300

    prueba: puedo crear un producto.

---
+++ Vuelto Inteligente/src/server/app/entities/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VueltoInteligente.Server.Application.Entities
+{
+    public class Producto
+    {
+        private string v;
+
+        public Producto(string v)
+        {
+            this.v = v;
+        }
+    }
+}
