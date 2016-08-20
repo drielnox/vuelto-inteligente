05816e47c31d8e8e3844fb1146a0db7f345a60d5
drielnox <bl4z3r@gmail.com>
2016-08-20 16:06:09 -0300

    prueba: puedo crear un cajero.

---
+++ Vuelto Inteligente/src/server/app/entities/Cajero.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VueltoInteligente.Server.Application.Entities
+{
+    public class Cajero
+    {
+    }
+}
